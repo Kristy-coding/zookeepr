8964926ac1911349545474240a32cc02372190b4
Kristy Zurmuhlen <kristyzurmuhlen@gmail.com>
2021-01-09 14:53:32 -0500

    requests

--- server.js
+++ server.js
@@ -51,7 +51,10 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
-
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
 // get() method requires two arguments. The first is a string that describes the route the client will have to fetch from. The second is a callback function that will execute every time that route is accessed with a GET request.
 //using the send() method from the res parameter (short for response) to send the string Hello! to our client.
 // then we changes send() to json to send large data 
@@ -63,6 +66,15 @@ app.get('/api/animals', (req, res) => {
         results = filterByQuery(req.query, results);
     }
     res.json(results)
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result)
+    } else {
+        res.send(404);
+    }
 });
 //chain the listen() method onto our server to make our server listen
 app.listen(PORT, () => {
